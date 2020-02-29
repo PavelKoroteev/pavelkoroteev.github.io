1de1179a97e8189d927c1854d37de41f1eacce33
PavelKoroteev <p_koroteev@bk.ru>
2020-02-29 14:40:16 +0300

    Try to fix listeners.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -19,20 +19,20 @@ export default class Calculator extends VueComponent<Props> {
         {/* <h1>{ this.msg }</h1> */}
 
         <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.zero]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.one]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.two]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.three]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.four]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.five]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.six]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.seven]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.eight]}></div>
-        <div v-on:click="console.log('1')" class={[styles['number-buttons'], styles.nine]}></div>
+        {/* <div v-on:click="someMethod" class={[styles['number-buttons'], styles.one]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.two]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.three]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.four]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.five]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.six]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.seven]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.eight]}></div>
+        <div v-on:click="someMethod" class={[styles['number-buttons'], styles.nine]}></div>
 
-        <div v-on:click="console.log('1')" class={[styles['action-buttons'], styles.clear]}></div>
-        <div v-on:click="console.log('1')" class={[styles['action-buttons'], styles.minus]}></div>
-        <div v-on:click="console.log('1')" class={[styles['action-buttons'], styles.plus]}></div>
-        <div v-on:click="console.log('1')" class={[styles['action-buttons'], styles.result]}></div>
+        <div v-on:click="someMethod" class={[styles['action-buttons'], styles.clear]}></div>
+        <div v-on:click="someMethod" class={[styles['action-buttons'], styles.minus]}></div>
+        <div v-on:click="someMethod" class={[styles['action-buttons'], styles.plus]}></div>
+        <div v-on:click="someMethod" class={[styles['action-buttons'], styles.result]}></div> */}
 
       </div>
     )
